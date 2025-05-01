9fe6031385a19071e980b22994b451af8922eba2
Imtiaz <imti.imtiaz2@gmail.com>
2025-05-01 10:44:20 -0700

    working on js built timer related functions

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Hello, World!");
+// Initalize 
+const pomodoro = document.getElementById("pomo");
+const long_break = document.getElementById("long");
+
+let timer = document.getElementById("timer");
+let timerStart = false;
+
+// Change display timer only when timer is not running
+pomodoro.addEventListener("click", () => {
+    if (timerStart) {
+        return;
+    }
+    timer.innerHTML = convertSecondsToTime(3600);
+});
+
+long_break.addEventListener("click", () => {
+    if (timerStart) {
+        return;
+    }
+    timer.innerHTML = convertSecondsToTime(1200);
+});
+
+function convertSecondsToTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  return `${String(minutes).padStart(2, "0")}:${String(remainingSeconds).padStart(2, "0")}`;
+}
+
