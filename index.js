78fc4215c4968605a4b492638a4d95830f1ec58e
Imtiaz <imti.imtiaz2@gmail.com>
2025-05-02 16:50:14 -0700

    built timer

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 const pomodoro = document.getElementById("pomo");
 const long_break = document.getElementById("long");
 
-let seconds = 0;
+let intervalId = null;
+let seconds = 3600;
 const timer = document.getElementById("timer");
 const timerControls = document.querySelector("#timer-controls");
 let timerStart = false;
+let onBreak = false;
 
 // Change display timer only when timer is not running
 pomodoro.addEventListener("click", () => {
@@ -24,22 +26,24 @@ long_break.addEventListener("click", () => {
     timer.innerHTML = convertSecondsToTime(seconds);
 });
 
-
+// idk copilot just did this
 function convertSecondsToTime(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainingSeconds = seconds % 60;
   return `${String(minutes).padStart(2, "0")}:${String(remainingSeconds).padStart(2, "0")}`;
 }
 
+// Adjust display of controls
+// fill when hover
 timerControls.addEventListener("mouseover", (event) => {
     if (!timerStart) {
         timerControls.src = "src/play-circle-fill.svg";
     } else {
         timerControls.src = "src/pause-circle-fill.svg";
     }
-    
 });
 
+// unfill when not hover
 timerControls.addEventListener("mouseout", (event) => {
     if (!timerStart) {
         timerControls.src = "src/play-circle.svg";
@@ -47,3 +51,43 @@ timerControls.addEventListener("mouseout", (event) => {
         timerControls.src = "src/pause-circle.svg";
     }
 });
+
+// Start timer when play button is clicked
+timerControls.addEventListener("click", () => {
+    // If timer is running, stop it
+    // and change icon to play
+    // vice versa
+
+    if (timerStart) {
+        timerStart = false;
+        clearInterval(intervalId);
+        timerControls.src = "src/play-circle-fill.svg";
+        console.log(timerStart);
+    } else {
+        timerStart = true;
+        updateTimer();
+        timerControls.src = "src/pause-circle-fill.svg";
+        console.log(timerStart);
+    }
+});
+
+// Update timer every second if timer is running
+function updateTimer() {
+    intervalId = setInterval( () => {
+        seconds--;
+        timer.innerHTML = convertSecondsToTime(seconds);
+
+        if (seconds == 0) {
+            if (onBreak) {
+                seconds = 3600;
+                onBreak = false;
+                timer.innerHTML = convertSecondsToTime(seconds);
+            } else {
+                seconds = 1200;
+                onBreak = true;
+                timer.innerHTML = convertSecondsToTime(seconds);
+            }
+        }
+    }, 1000)
+}
+
