87a75cd45cf6afb23192eb7641a6719f8890a727
Imtiaz Ahmed <imti.imtiaz2@gmail.com>
2026-05-12 20:43:43 -0700

    new start

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('api', {
-  openAniListWindow: () => ipcRenderer.invoke('open-anilist-window'),
-  loginWithAniList: () => ipcRenderer.invoke('oauth-login')
-})
--- renderer.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('openAniListBtn').addEventListener('click', () => {
-  window.api.openAniListWindow()
-})
