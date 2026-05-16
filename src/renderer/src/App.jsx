ef1f218cae2423a8d25df75ffcac5abeb762909d
Imtiaz Ahmed <imti.imtiaz2@gmail.com>
2026-05-16 16:22:07 -0700

    implement navbar

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,32 +1,38 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
+import './index.css'
 
 function App() {
   const ipcHandle = () => window.electron.ipcRenderer.send('ping')
 
   return (
     <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
+      <div className='navbar bg-base-100 shadow-sm'>
+        <div className='navbar-start'>
+          <div className='dropdown'>
+            <div tabIndex={0} role='button' className='btn btn-ghost btn-circle'>
+              <svg className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
+            </div>
+            <ul
+              tabIndex='-1'
+              className='menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow'
+            >
+              <li>
+                <a href='#'>Item 1</a>
+              </li>
+              <li>
+                <a href='#'>Item 2</a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
+        <div className='navbar-center'>
+          <a className='btn btn-ghost text-xl'>My App</a>
+        </div>
+        <div className='navbar-end'>
+          <button className='btn' onClick={ipcHandle}>
+            Send Ping
+          </button>
         </div>
       </div>
-      <Versions></Versions>
     </>
   )
 }
